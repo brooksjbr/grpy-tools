606851003dddc0f255034aa98b1a94b82a82f356
brooksjbr <brooksjbr@protonmail.ch>
2025-01-19 14:55:55 -0500

    feat: update test to supportother attribute tests

--- tests/test_bootstrap_path.py
+++ tests/test_bootstrap_path.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from unittest.mock import Mock, patch
+from unittest.mock import patch
 
 import pytest
 from pydantic import ValidationError
@@ -7,16 +7,16 @@ from pydantic import ValidationError
 from src.grpy.dev_tools.bootstrap_path import BootstrapPath
 
 
-def test_bootstrap_path():
-    mock_path = Mock(spec=Path)
+def test_bootstrap_path_attributes():
+    mock_working_path = Path("/mock/user/projects/mock-project")
     with patch.object(Path, "exists", return_value=True):
-        bootstrap = BootstrapPath(working_path=mock_path)
+        bootstrap = BootstrapPath(working_path=mock_working_path)
     assert isinstance(bootstrap, BootstrapPath)
-    assert bootstrap.working_path == mock_path
+    assert bootstrap.working_path == mock_working_path
 
 
 def test_boostrap_path_validation():
     path = Path("/this/path/does/not/exist")
     with pytest.raises(ValidationError):
         bp = BootstrapPath(working_path=path)
-        assert bp.working_path == path
+        assert bp.working_path is None
